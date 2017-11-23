e87e6c3b2a5662751a19848768bc6e37b2ea55ce
Diannie <mb15004@ues.edu.sv>
2017-11-23 15:40:04 -0600

    Interfaz terminada

---
+++ src/main/java/com/example/demo/FpiParcial3Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpiParcial3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpiParcial3Application.class, args);
+	}
+}
