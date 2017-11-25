b40f7450df21b8aab729cb6d0190b4d799ea6513
Diannie <mb15004@ues.edu.sv>
2017-11-25 15:16:46 -0600

    trabajando en login

--- src/main/resources/static/js/master.js
+++ src/main/resources/static/js/master.js
@@ -1,31 +1,19 @@
 document.getElementById('loginForm').onsubmit = function(e){
-
+  alert("ok");
+  e.preventDefault();
   var email = document.getElementById("email").value;
-	var pass = document.getElementById("pass").value;
+  var pass = document.getElementById("pass").value;
+  var obj = {"id": 0, "email": email, "name": "", "gender": "",  "pass": pass};
   var userRequest = new XMLHttpRequest();
-	  userRequest.onreadystatechange = function() {
-		  alert("ok");
-		  if (this.readyState == 4 && this.status == 200) {
-			  var respuesta = JSON.parse(this.responseText);
-			  
-			  if(respuesta.id===null){
-				  e.preventDefault();
-				  document.getElementById('errores').value = "Contraseña o Correo incorrectos";
-				  document.getElementById('errores').style.display = "block";
-			  }else{
-
-			  var date = new Date();
-			  date.setTime(date.getTime()+(3*60*1000));
-			  var expires = "; expires="+date.toGMTString();
-			  document.cookie = "usuarioLogueado="+[respuesta.id,respuesta.name,respuesta.email]+expires;
-        location.href= "../pokedex.html";
-        document.getElementById('navSigin').style.display = "none";
-        document.getElementById('navProfile').style.display = "block";
-			  }
-		  }
-	  };
-	  userRequest.open('POST', '/singin');
-	  userRequest.setRequestHeader("Content-Type", "application/json");
-	  userRequest.setRequestHeader("Accept", "application/json");
-	  userRequest.send(JSON.stringify({"id": 0, "name": "", "email": email, "contrasena": pass}));
+    userRequest.onreadystatechange = function() {
+      alert("ok");
+      if (this.readyState == 4 && this.status == 200) {
+        alert("email");
+        alert("pass");
+      }
+    };
+    userRequest.open('POST', 'http://192.168.1.23:8080/sigin');
+    userRequest.setRequestHeader("Content-Type", "application/json");
+    userRequest.setRequestHeader("Accept", "application/json");
+    userRequest.send(JSON.stringify(obj));
 }
